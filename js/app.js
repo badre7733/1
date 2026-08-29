160ebba45c44ec1241d91b8ff89555f4966344e8
badredin <badredinazzahraoui74@gmail.com>
2026-08-29 15:44:28 +0100

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // =====================================================
 // Checkout
 const MP_API_URL = "/api/create-preference";
-const MP_USE_SANDBOX = true;  // pruebas
+const MP_USE_SANDBOX = false; 
 const MERCADOPAGO_LINK = "";
 // Ejemplo: const MERCADOPAGO_LINK = "https://mpago.la/xxxxx";
 // =====================================================
